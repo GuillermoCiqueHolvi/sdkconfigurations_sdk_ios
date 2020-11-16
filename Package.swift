8a3f630ad5a82edaa894b68a795f018aa3553dee
Guillermo Cique <guillermo@holvi.com>
2020-11-16 14:14:52 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 let package = Package(
     name: "ZendeskSDKConfigurations",
     products: [
-        .library(name: "ZendeskSDKConfigurations", targets: ["SDKConfigurations"])
+        .library(name: "ZendeskSDKConfigurations", targets: ["ZendeskSDKConfigurations"])
     ],
     targets: [
         .binaryTarget(
-            name: "SDKConfigurations",
+            name: "ZendeskSDKConfigurations",
             url: "https://zendesk.jfrog.io/zendesk/libs-releases-local/ios/zendesk/5.2.4/SDKConfigurations/1.1.6/SDKConfigurations.zip",
             checksum: "dbad6be9cafb72327e2a3912f28a88c5b5d65230546ab015d8cec8772949c348"
         )
